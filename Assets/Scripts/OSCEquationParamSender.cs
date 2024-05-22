50cd21c2786cdbe482915c41672fa977773c35b1
Alex Andrix <alex@andrix.fr>
2024-05-22 19:29:05 +0200

    Add complex dashboard variables display behaviour

--- Assets/Scripts/OSCEquationParamSender.cs
+++ Assets/Scripts/OSCEquationParamSender.cs
@@ -2,9 +2,11 @@
  * ANDRIX © 2024, based on the extOSC library (Vladimir Sigalkin), MIT License
  */
 
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.VFX;
 using extOSC;
 using TMPro;
@@ -14,71 +16,168 @@ public class OSCEquationParamSender : MonoBehaviour
 
 	[Header("OSC Settings")]
 	public OSCTransmitter transmitter;
+
+	[Header("Local bindings")]
 	public VisualEffect equationVFX;
 
+	// Dashboard variables
+	public TextMeshProUGUI slot1;
+	public TextMeshProUGUI slot2;
+	public TextMeshProUGUI slot3;
+	public TextMeshProUGUI slot4;
+	public TextMeshProUGUI slot5;
+	public TextMeshProUGUI slot6;
+	public TextMeshProUGUI slot7;
+	public TextMeshProUGUI slot8;
+	public TextMeshProUGUI slot9;
+	public TextMeshProUGUI slot10;
+
+	// Equation image
+	public Image _equation;
+
+	// Axes coordinates canvas
+	public Canvas _coordinatesCanvas;
+	private TextMeshProUGUI[] _coordinates;
+
+	// Camera
+	public RotAround cam;
+
 	// Equation variables
+	[Header("Equation variables")]
 	[Range(0.0f, 10.0f)]
 	public float k0 = 1.0f;
-	private float lastK0 = 1.0f;
+	public PlayVariable K0;
+	//private float lastK0 = 1.0f;
+	//private float k0time = 0.0f;
 
 	[Range(0.0f, 10.0f)]
-	public float E0 = 1.0f;
-	private float lastE0 = 1.0f;
-
-	[Range(0.0f, 10.0f)]
-	public float E4 = 1.0f;
-	private float lastE4 = 1.0f;
-
-	[Range(-3.0f, 3.0f)]
-	public float nu = 1.0f;
-	private float lastNu = 1.0f;
+	public float e0 = 1.0f;
+	public PlayVariable E0;
+	//private float lastE0 = 1.0f;
+	//private float E0time = 0.0f;
 
 	[Range(-3.0f, 3.0f)]
 	public float lambda = 1.0f;
-	private float lastLambda = 1.0f;
+	public PlayVariable Lambda;
+	// private float lastLambda = 1.0f;
+	// private float lambdaTime = 0.0f;
 
-	[Range(0.0f, 3.0f)]
+	[Range(-3.0f, 3.0f)]
+	public float nu = 1.0f;
+	
+	public PlayVariable Nu;
+	// private float lastNu = 1.0f;
+	// private float nuTime = 0.0f;
+
+	[Range(0.0f, 2.0f)]
 	public float eta = 0.0f;
-	private float lastEta = 0.0f;
+	public PlayVariable Eta;
+	// private float lastEta = 0.0f;
+	// private float etaTime = 0.0f;
 
 	[Range(-3.0f, 3.0f)]
 	public float a = 1.0f;
-	private float lastA = 1.0f;
+	public PlayVariable A;
+	// private float lastA = 1.0f;
+	// private float aTime = 0.0f;
 
 	[Range(0.0f, 3.0f)]
 	public float b = 1.0f;
-	private float lastB = 1.0f;
+	public PlayVariable B;
+	// private float lastB = 1.0f;
+	// private float bTime = 0.0f;
 
 	[Range(0.0f, 3.0f)]
 	public float alpha = 1.0f;
-	private float lastAlpha = 1.0f;
+	public PlayVariable Alpha;
+	// private float lastAlpha = 1.0f;
+	// private float alphaTime = 0.0f;
 
 	[Range(0.0f, 3.0f)]
 	public float beta = 1.0f;
-	private float lastBeta = 1.0f;
+	public PlayVariable Beta;
+	// private float lastBeta = 1.0f;
+	// private float betaTime = 0.0f;
 
 	[Range(0.0f, 5.0f)]
 	public float turbulence = 0.0f;
-	private float lastTurbulence = 0.0f;
+	public PlayVariable Turbulence;
+	// private float lastTurbulence = 0.0f;
+	// private float turbulenceTime = 0.0f;
 
 	[Range(0.0f, 5.0f)]
 	public float incrementSpeed = 1.5f;
-	private float lastIncrementSpeed = 1.5f;
+	// private float lastIncrementSpeed = 1.5f;
+
+	private PlayVariable[] playVariables;
+	private PlayVariable[] sortedVariables;
+
+
+	[Header("Visual controls")]
+	[Range(0.0f, 1.0f)]
+	public float axesOpacity = 1.0f;
+	private float lastAxesOpacity = 1.0f;
 
 	[Range(0.0f, 1.0f)]
-	public float opacity = 1.0f;
+	public float coordinatesOpacity = 1.0f;
+	private float lastCoordinatesOpacity = 1.0f;
 
-	// Variables displayed text
-	public TextMeshProUGUI _E0;
-	public TextMeshProUGUI _E4;
-	public TextMeshProUGUI _nu;
-	public TextMeshProUGUI _lambda;
-	public TextMeshProUGUI _k0;
-	public TextMeshProUGUI _eta;
+	[Range(0.0f, 1.0f)]
+	public float dashboardOpacity = 1.0f;
+	public bool dashboardOrderToggle = false;
+	public bool dashboardFadingToggle = false;
+	private bool lastDashboardOrderToggle = false;
+	private bool lastDashboardFadingToggle = false;
+
+	[Range(0.0f, 1.0f)]
+	public float equationOpacity = 1.0f;
+	private float lastEquationOpacity = 1.0f;
+
+	[Range(-0.5f, 0.5f)]
+	public float camSpeed = 0.07f;
+	private float lastCamSpeed = 0.07f;
+
+	[Range(-720.0f, 720.0f)]
+	public float camBaseAngle = 0.0f;
+	private float lastCamBaseAngle = 0.0f;
+
+	[Range(-2.0f, 8.0f)]
+	public float camRadius = 4.0f;
+	private float lastCamRadius = 4.0f;
+
 
 	protected void Start()
 	{
-		
+		// Get all child coordinates from Coordinates Canvas
+		_coordinates = _coordinatesCanvas.GetComponentsInChildren<TextMeshProUGUI>();
+
+		// Store all play variables in an array
+		K0 = new PlayVariable("k0", k0);
+		E0 = new PlayVariable("E0", e0);
+		Lambda = new PlayVariable("lambda", lambda);
+		Nu = new PlayVariable("nu", nu);
+		Eta = new PlayVariable("eta", eta);
+		A = new PlayVariable("a", a);
+		B = new PlayVariable("b", b);
+		Alpha = new PlayVariable("alpha", alpha);
+		Beta = new PlayVariable("beta", beta);
+		Turbulence = new PlayVariable("turbulence", turbulence);
+
+		playVariables = new PlayVariable[10];
+		playVariables[0] = K0;
+		playVariables[1] = E0;
+		playVariables[2] = Lambda;
+		playVariables[3] = Nu;
+		playVariables[4] = Eta;
+		playVariables[5] = A;
+		playVariables[6] = B;
+		playVariables[7] = Alpha;
+		playVariables[8] = Beta;
+		playVariables[9] = Turbulence;
+
+		// Prepare array that will order displayed value based on last update time
+		sortedVariables = new PlayVariable[10];
+		playVariables.CopyTo(sortedVariables, 0);
 	}
 
 	void Update()
@@ -89,21 +188,20 @@ public class OSCEquationParamSender : MonoBehaviour
 	void Check()
 	{
 		string variableName = "";
-		float variableValue = 1;
+		float variableValue = 1.0f;
 
-		if (k0 != lastK0) { lastK0 = k0; variableName = "k0"; variableValue = k0; }
-		else if (E0 != lastE0) { lastE0 = E0; variableName = "E0"; variableValue = E0; }
-		else if (E4 != lastE4) { lastE4 = E4; variableName = "E4"; variableValue = E4; }
-		else if (nu != lastNu) { lastNu = nu; variableName = "nu"; variableValue = nu; }
-		else if (lambda != lastLambda) { lastLambda = lambda; variableName = "lambda"; variableValue = lambda; }
-		else if (eta != lastEta) { lastEta = eta; variableName = "eta"; variableValue = eta; }
-		else if (a != lastA) { lastA = a; variableName = "a"; variableValue = a; }
-		else if (b != lastB) { lastB = b; variableName = "b"; variableValue = b; }
-		else if (alpha != lastAlpha) { lastAlpha = alpha; variableName = "alpha"; variableValue = alpha; }
-		else if (beta != lastBeta) { lastBeta = beta; variableName = "beta"; variableValue = beta; }
-		else if (turbulence != lastTurbulence) { lastTurbulence = turbulence; variableName = "turbulence"; variableValue = turbulence; }
-		else if (incrementSpeed != lastIncrementSpeed) { lastIncrementSpeed = incrementSpeed; variableName = "incrementSpeed"; variableValue = incrementSpeed; }
+		if (k0 != playVariables[0].value) 		   		    { playVariables[0].update(k0); variableName = "k0"; variableValue = k0; }
+		else if (e0 != playVariables[1].value)				{ playVariables[1].update(e0); variableName = "E0"; variableValue = e0; }
+		else if (lambda != playVariables[2].value) 			{ playVariables[2].update(lambda); variableName = "lambda"; variableValue = lambda; }
+		else if (nu != playVariables[3].value) 		 		{ playVariables[3].update(nu); variableName = "nu"; variableValue = nu; }
+		else if (eta != playVariables[4].value) 		 	{ playVariables[4].update(eta); variableName = "eta"; variableValue = eta; }
+		else if (a != playVariables[5].value) 			 	{ playVariables[5].update(a); variableName = "a"; variableValue = a; }
+		else if (b != playVariables[6].value) 				{ playVariables[6].update(b); variableName = "b"; variableValue = b; }
+		else if (alpha != playVariables[7].value) 	 		{ playVariables[7].update(alpha); variableName = "alpha"; variableValue = alpha; }
+		else if (beta != playVariables[8].value) 	 		{ playVariables[8].update(beta); variableName = "beta"; variableValue = beta; }
+		else if (turbulence != playVariables[9].value) 		{ playVariables[9].update(turbulence); variableName = "turbulence"; variableValue = turbulence; }
 
+		// Handle variable update if one value changed
 		if (variableName.Length > 0)
 		{
 			// Send OSC message
@@ -115,24 +213,154 @@ public class OSCEquationParamSender : MonoBehaviour
 			// Change local VFX value
 			equationVFX.SetFloat(variableName, variableValue);
 
-			// Update display TextMeshPro values
-			_E0.text = "E0 = " + E0.ToString();
-			_E4.text = "E4 = " + E4.ToString();
-			_nu.text = "nu = " + nu.ToString();
-			_lambda.text = "lambda = " + lambda.ToString();
-			_k0.text = "k0 = " + k0.ToString();
-			_eta.text = "eta = " + eta.ToString();
+			// Update dashboard values (order by last update time)
+			Array.Sort<PlayVariable>(sortedVariables);
 
+			UpdateDashboardText();
 		}
 
-		// Update colors (opacity)
-		_E0.color = new Color(1, 1, 1, opacity);
-		_E4.color = new Color(1, 1, 1, opacity);
-		_nu.color = new Color(1, 1, 1, opacity);
-		_lambda.color = new Color(1, 1, 1, opacity);
-		_k0.color = new Color(1, 1, 1, opacity);
-		_eta.color = new Color(1, 1, 1, opacity);
+		// Handle update if one dashboard mode toggles changed
+		if (lastDashboardFadingToggle != dashboardFadingToggle || lastDashboardOrderToggle != dashboardOrderToggle)
+		{
+			lastDashboardFadingToggle = dashboardFadingToggle;
+			lastDashboardOrderToggle = dashboardOrderToggle;
+
+			UpdateDashboardText();
+		}
+
+		// Update opacities if one value changed
+		if (lastAxesOpacity != axesOpacity || lastCoordinatesOpacity != coordinatesOpacity || lastEquationOpacity != equationOpacity)
+		{
+			UpdateOpacities();
+		}
+		
+		// Update camera variables if one value changed
+		if (lastCamSpeed != camSpeed ||	lastCamBaseAngle != camBaseAngle ||	lastCamRadius != camRadius)
+		{
+			UpdateCamera();
+		}
+
+		// Always update dashboard text opacities
+		UpdateDashboardOpacities();
 
 	}
 
+	void UpdateOpacities()
+	{
+		// Update VFX axes opacity
+		equationVFX.SetFloat("axesOpacity", axesOpacity);
+		if (axesOpacity == 0)
+		{
+			equationVFX.SetBool("toggleAxes", false);
+		}
+		else
+		{
+			equationVFX.SetBool("toggleAxes", true);
+		}
+		
+		// Update coordinates opacity
+		foreach (TextMeshProUGUI _coord in _coordinates)
+		{
+			_coord.color = new Color(1, 1, 1, coordinatesOpacity);
+		}
+
+		// Update equation image opacity
+		_equation.color = new Color(1, 1, 1, equationOpacity);
+
+		// Update value buffers
+		lastAxesOpacity = axesOpacity;
+		lastCoordinatesOpacity = coordinatesOpacity;
+		lastEquationOpacity = equationOpacity;
+
+	}
+
+	void UpdateCamera()
+	{
+		// Update camera variables
+		cam.revolutionSpeed = camSpeed;
+		cam.baseRotAngleY = camBaseAngle;
+		cam.radius = camRadius;
+
+		// Update value buffers
+		lastCamSpeed = camSpeed;
+		lastCamBaseAngle = camBaseAngle;
+		lastCamRadius = camRadius;
+	}
+
+	void UpdateDashboardText()
+	{
+		// Update text content
+		if (dashboardOrderToggle)
+		{
+			slot1.text = sortedVariables[0].getText();
+			slot2.text = sortedVariables[1].getText();
+			slot3.text = sortedVariables[2].getText();
+			slot4.text = sortedVariables[3].getText();
+			slot5.text = sortedVariables[4].getText();
+			slot6.text = sortedVariables[5].getText();
+			slot7.text = sortedVariables[6].getText();
+			slot8.text = sortedVariables[7].getText();
+			slot9.text = sortedVariables[8].getText();
+			slot10.text = sortedVariables[9].getText();
+		}
+		else
+		{
+			slot1.text = playVariables[0].getText();
+			slot2.text = playVariables[1].getText();
+			slot3.text = playVariables[2].getText();
+			slot4.text = playVariables[3].getText();
+			slot5.text = playVariables[4].getText();
+			slot6.text = playVariables[5].getText();
+			slot7.text = playVariables[6].getText();
+			slot8.text = playVariables[7].getText();
+			slot9.text = playVariables[8].getText();
+			slot10.text = playVariables[9].getText();
+		}
+
+	}
+
+	void UpdateDashboardOpacities()
+	{
+		// Full color (white) or slowly fading? Raw (unordered) or reordered?
+		if (dashboardFadingToggle && dashboardOrderToggle)
+		{
+			slot1.color = new Color(1, 1, 1, sortedVariables[0].getOpacity() * dashboardOpacity);
+			slot2.color = new Color(1, 1, 1, sortedVariables[1].getOpacity() * dashboardOpacity);
+			slot3.color = new Color(1, 1, 1, sortedVariables[2].getOpacity() * dashboardOpacity);
+			slot4.color = new Color(1, 1, 1, sortedVariables[3].getOpacity() * dashboardOpacity);
+			slot5.color = new Color(1, 1, 1, sortedVariables[4].getOpacity() * dashboardOpacity);
+			slot6.color = new Color(1, 1, 1, sortedVariables[5].getOpacity() * dashboardOpacity);
+			slot7.color = new Color(1, 1, 1, sortedVariables[6].getOpacity() * dashboardOpacity);
+			slot8.color = new Color(1, 1, 1, sortedVariables[7].getOpacity() * dashboardOpacity);
+			slot9.color = new Color(1, 1, 1, sortedVariables[8].getOpacity() * dashboardOpacity);
+			slot10.color = new Color(1, 1, 1, sortedVariables[9].getOpacity() * dashboardOpacity);
+		}
+		else if (dashboardFadingToggle && !dashboardOrderToggle)
+		{
+			slot1.color = new Color(1, 1, 1, playVariables[0].getOpacity() * dashboardOpacity);
+			slot2.color = new Color(1, 1, 1, playVariables[1].getOpacity() * dashboardOpacity);
+			slot3.color = new Color(1, 1, 1, playVariables[2].getOpacity() * dashboardOpacity);
+			slot4.color = new Color(1, 1, 1, playVariables[3].getOpacity() * dashboardOpacity);
+			slot5.color = new Color(1, 1, 1, playVariables[4].getOpacity() * dashboardOpacity);
+			slot6.color = new Color(1, 1, 1, playVariables[5].getOpacity() * dashboardOpacity);
+			slot7.color = new Color(1, 1, 1, playVariables[6].getOpacity() * dashboardOpacity);
+			slot8.color = new Color(1, 1, 1, playVariables[7].getOpacity() * dashboardOpacity);
+			slot9.color = new Color(1, 1, 1, playVariables[8].getOpacity() * dashboardOpacity);
+			slot10.color = new Color(1, 1, 1, playVariables[9].getOpacity() * dashboardOpacity);
+		}
+		else
+		{
+			slot1.color = new Color(1, 1, 1, 1 * dashboardOpacity);
+			slot2.color = new Color(1, 1, 1, 1 * dashboardOpacity);
+			slot3.color = new Color(1, 1, 1, 1 * dashboardOpacity);
+			slot4.color = new Color(1, 1, 1, 1 * dashboardOpacity);
+			slot5.color = new Color(1, 1, 1, 1 * dashboardOpacity);
+			slot6.color = new Color(1, 1, 1, 1 * dashboardOpacity);
+			slot7.color = new Color(1, 1, 1, 1 * dashboardOpacity);
+			slot8.color = new Color(1, 1, 1, 1 * dashboardOpacity);
+			slot9.color = new Color(1, 1, 1, 1 * dashboardOpacity);
+			slot10.color = new Color(1, 1, 1, 1 * dashboardOpacity);
+		}
+
+	}
 }
---
+++ Assets/Scripts/PlayVariable.cs
@@ -0,0 +1,45 @@
+/**
+ * Custom class to store Equation Variable information with extra details
+ * ANDRIX © 2024
+ */
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayVariable : IComparable<PlayVariable>
+{
+	public string name;
+    public float value;
+    public float time;
+    private float duration; // Opacity fadeout duration
+
+    public PlayVariable(string name, float value)
+    {
+        this.name = name;
+        this.value = value;
+        this.time = Time.time;
+        this.duration = 4.0f;
+    }
+
+    public void update(float newValue)
+    {
+        this.value = newValue;
+        this.time = Time.time;
+    }
+
+    public string getText()
+    {
+        return this.name + " = " + this.value;
+        // return this.name + " = " + this.value.ToString();
+    }
+
+    public float getOpacity()
+    {
+        return Mathf.Clamp(1.0f - (Time.time - this.time) / this.duration, 0, 1.0f);
+    }
+
+    public int CompareTo(PlayVariable otherPlayVar)
+    {
+        return -this.time.CompareTo(otherPlayVar.time);
+    }
+}
